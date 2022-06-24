dd598e2846cb483a7cf77c87fd33cc656aff9bfd
Yinon Ozery <yinonozery@gmail.com>
2022-06-24 17:18:05 +0300

    Fix MongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {
+
+mongoose.connect('mongodb+srv://best2watch-user:<password>@best2watch.3tv2j.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
